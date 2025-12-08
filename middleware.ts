3b67c24e71ed08f32f8b17e30a553b2171cfa1f7
Daniel <daniel.leebl@gmail.com>
2025-12-08 13:42:36 +0800

    fix: Exclude /impersonate from i18n middleware to resolve 404 issue

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: '/((?!api|static|.*\\..*|_next).*)'
+    matcher: '/((?!api|static|impersonate|.*\\..*|_next).*)'
 };
